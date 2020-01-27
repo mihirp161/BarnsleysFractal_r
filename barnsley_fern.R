27e8a46d1449bbe60a57a1f576ecaed65d278a8d
Mihir Patel <47681434+ToadHanks@users.noreply.github.com>
2020-01-27 09:51:41 -0500

    Add files via upload

--- barnsley_fern.R
+++ barnsley_fern.R
@@ -1,36 +1,48 @@
-library(viridis)
+library(viridis) #just for colouring purpose
 
-max_itr<-1000000
+max_itr<-1000000 #number of iterations
 
-x <- rep(0, times=max_itr)
-y <- x
+x <- rep(0, times=max_itr) #make vector of 0s
+y <- x #copy the vector
 
-for (i in 2:(max_itr)) {
-  rand=runif(1, 0, 100)
+for (n in 2:(max_itr)) { #run the iteration frm
   
-  if (rand<1) {
-    x[i]<-0
-    y[i]<-0.16*y[i-1]
+  rand_num=runif(1, 0, 100) #generate a random number between 0-100
+  # starting at x = 0, y = 0
+  #f1: xn+1 = 0 (chosen 1% of the time)
+  #    yn+1 = 0.16 yn
+  #f2: xn+1 = ???0.15 xn + 0.28 yn (chosen 7% of the time)
+  #    yn+1 = 0.26 xn + 0.24 yn + 0.44
+  #f3: xn+1 = 0.2 xn ??? 0.26 yn (chosen 14% of the time)
+  #    yn+1 = 0.23 xn + 0.22 yn + 1.6
+  #f4: xn+1 = 0.85 xn + 0.04 yn (chosen 85% of the time)
+  #    yn+1 = ???0.04 xn + 0.85 yn + 1.6
+  
+  # I have subtracted 1 at every iteration from n so I dont have to add
+  # 1 to f(x), basically moved subtracted from the both sides
+  
+  if (rand_num < 1) { 
+    x[n]<- 0
+    y[n]<- 0.16*y[n-1]
   }
-  
-  else if (rand<7){
-    x[i]<- -0.15*x[i-1]+0.28*y[i-1]
-    y[i]<-0.26*x[i-1]+0.24*y[i-1]+0.44
+  else if (rand_num < 7){
+    x[n]<- -0.15*x[n-1]+0.28*y[n-1]
+    y[n]<- 0.26*x[n-1]+0.24*y[n-1]+0.44
   }
-  
-  else if (rand<14){
-    x[i]<-0.2*x[i-1]-0.26*y[i-1]
-    y[i]<-0.23*x[i-1]+0.22*y[i-1]+1.6
+  else if (rand_num < 14){
+    x[n]<- 0.2*x[n-1]-0.26*y[n-1]
+    y[n]<- 0.23*x[n-1]+0.22*y[n-1]+1.6
   }
-  
   else {
-    x[i]<-0.85*x[i-1]+0.04*y[i-1]
-    y[i]<- -0.04*x[i-1]+0.85*y[i-1]+1.6
+    x[n]<- 0.85*x[n-1]+0.04*y[n-1]
+    y[n]<- -0.04*x[n-1]+0.85*y[n-1]+1.6
   }
+  
 }
 
-color <- viridis::viridis(40)
+color <- viridis::viridis(40) #color palette
 
+#Saving the plot
 png(filename="fern.png", bg= "floralwhite",height = 1920, width = 1080, units = "px", res= 150) 
 plot(x,y, pch='.', xlab= "", ylab = "", col= color)
 title(xlab = "Span", ylab = "Arch", col.lab ="indianred4")
